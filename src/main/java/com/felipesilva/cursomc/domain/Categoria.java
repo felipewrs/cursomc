3b7766bec566ba41fd29c36ba0801bf74a3a8413
Felipe Silva <fsilva9222@gmail.com>
2025-02-28 17:57:50 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/felipesilva/cursomc/domain/Categoria.java
+++ src/main/java/com/felipesilva/cursomc/domain/Categoria.java
@@ -3,17 +3,23 @@ package com.felipesilva.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Categoria implements Serializable
-{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
+
 	public Categoria() {
 	}
-	
+
 	public Categoria(Integer id, String nome) {
 		this.id = id;
 		this.nome = nome;
