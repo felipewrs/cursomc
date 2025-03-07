1143ea33a569fb8e970b3b8e53e1648a46605fca
Felipe Silva <fsilva9222@gmail.com>
2025-03-07 16:51:33 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/felipesilva/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/felipesilva/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.felipesilva.cursomc.services.exceptions.DataIntegrityException;
 import com.felipesilva.cursomc.services.exceptions.ObjectNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,4 +20,12 @@ public class ResourceExceptionHandler {
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
 }
